59c9967a0e5f701478ac1e3e580c195d851681c9
Martin Jonasson <grapefrukt@grapefrukt.com>
2014-04-24 11:53:05 +0200

    Building for armv7 as well

--- project/android.sh
+++ project/android.sh
@@ -1,3 +1,5 @@
 rm -rf "obj"
 echo "Compiling for armv6"
-haxelib run hxlibc Build.xml -Dandroid -debug
+haxelib run hxlibc Build.xml -Dandroid
+echo "Compiling for armv7"
+haxelib run hxlibc Build.xml -Dandroid -DHXCPP_ARMV7
